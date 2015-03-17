83cbb7c693f1400cf666e007c617f84fba297cdf
MBurwick <mburwick@gmail.com>
2015-03-16 22:09:17 -0400

    copy packer-windows-eval directory with .json files for eval 2012 machine

---
+++ packer-windows-eval/scripts/chocopacks.bat
@@ -0,0 +1,16 @@
+:: Ensure C:\Chocolatey\bin is on the path
+set /p PATH=<C:\Windows\Temp\PATH
+
+:: Install all the things; for example:
+cmd /c choco install 7zip
+cmd /c choco install notepadplusplus
+cmd /c choco install adobereader
+cmd /c choco install firefoxesr
+cmd /c choco install googlechrome
+cmd /c choco install libreoffice
+cmd /c choco install putty
+cmd /c choco install winscp
+cmd /c choco install ccleaner
+cmd /c choco install thunderbird
+cmd /c choco install openssl.light
+cmd /c choco install lastpass
